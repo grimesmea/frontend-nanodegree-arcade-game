92662e642ddafd77ea38f767227d7eb353b205d5
mary <grimesmea@gmail.com>
2014-12-08 16:10:13 -0500

    Change name of allEnemies to enemies in engine.js

--- js/engine.js
+++ js/engine.js
@@ -91,7 +91,7 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
+        enemies.forEach(function(enemy) {
             enemy.update(dt);
         });
         player.update();
@@ -148,7 +148,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
+        enemies.forEach(function(enemy) {
             enemy.render();
         });
 
