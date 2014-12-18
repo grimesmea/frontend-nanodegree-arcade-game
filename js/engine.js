1add3f74fcba9bbea8035e52dfb731ab9f79c2c0
mary <grimesmea@gmail.com>
2014-12-18 16:08:04 -0500

    Make dt Engine property available globally

--- js/engine.js
+++ js/engine.js
@@ -178,9 +178,10 @@ var Engine = (function(global) {
     ]);
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
+    /* Assign the canvas' context object and dt property to the global variable (the window
      * object when run in a browser) so that developer's can use it more easily
      * from within their app.js files.
      */
     global.ctx = ctx;
+    global.dt = main.dt;
 })(this);
