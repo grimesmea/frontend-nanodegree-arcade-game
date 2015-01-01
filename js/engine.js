d9afe5fe4f542de1856357268a8a36cc1f3c18bb
mary <grimesmea@gmail.com>
2015-01-01 16:29:02 -0500

    Clear canvas upon render

--- js/engine.js
+++ js/engine.js
@@ -103,6 +103,8 @@ var Engine = (function(global) {
    * they are just drawing the entire screen over and over.
    */
   function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     /* This array holds the relative URL to the image used
      * for that particular row of the game level.
      */
